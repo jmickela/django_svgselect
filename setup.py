d65b8f5a1b50917e858d64cfe47a401cebf401a2
Jason Mickela <jason@rootid.in>
2016-04-10 01:02:45 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,56 @@
+"""A setuptools based setup module.
+See:
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django_svgselect',
+    version='0.9.0b1',
+
+    description='A Django form widget that uses SVG files in place of Select widgets.',
+    long_description=long_description,
+
+    url='https://github.com/jmickela/django_svgselect',
+
+    author='Jason Mickela',
+    author_email='jason@rootid.in',
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='django form widget svg',
+
+    packages=get_packages('django_svgselect'),
+    install_requires=['django'],
+)
